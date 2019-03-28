2d4b3ef99b89f8c72fbbaf568827220171cb02a7
Grant Singleton <grantsingleton@tamu.edu>
2019-03-28 00:20:02 -0500

    Update Piece.java

--- src/Piece.java
+++ src/Piece.java
@@ -3,12 +3,39 @@ public class Piece {
 
 	private String color;
 	private String name;
+	private int row;
+	private int column;
 	
-	public Piece(String color, String name) {
-		this.color = color;
-		this.name = name;
+	public Piece() {
+		color = "red";
+		name = "none";
+		row = 100;
+		column = 100;
 	}
 	
+	public Piece(String color, String name, int row, int column) {
+		this.color = color;
+		this.name = name;
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
 	public String getColor() {
 		return color;
 	}
@@ -23,5 +50,17 @@ public class Piece {
 
 	public void setColor(String color) {
 		this.color = color;
+	}
+	
+	public void empty() {
+		color = "red";
+		name = "none";
+	}
+	
+	public boolean isEmpty() {
+		if (name == "none") {
+			return true;
+		}
+		return false;
 	}
 }
