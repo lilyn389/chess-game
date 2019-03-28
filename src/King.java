98cf098e7fd3814afe5638987551318eb1a5088f
Grant Singleton <grantsingleton@tamu.edu>
2019-03-28 00:18:49 -0500

    Update King.java

--- src/King.java
+++ src/King.java
@@ -3,13 +3,9 @@ import javax.swing.ImageIcon;
 public class King extends Piece {
 	
 	private ImageIcon king;
-	private int row;
-	private int column;
 	
 	public King(String color, String name, int row, int column) {
-		super(color, name);
-		this.row = row;
-		this.column = column;
+		super(color, name, row, column);
 		if (color == "white") {
 			king = new ImageIcon("white_king.png");
 		}
@@ -25,26 +21,10 @@ public class King extends Piece {
 	public void setRookIcon(ImageIcon king) {
 		this.king = king;
 	}
-
-	public int getRow() {
-		return row;
-	}
-
-	public void setRow(int row) {
-		this.row = row;
-	}
-
-	public int getColumn() {
-		return column;
-	}
-
-	public void setColumn(int column) {
-		this.column = column;
-	}
 	
 	public boolean isValidMove(int x, int y) {
 	
-		return false;
+		return true;
 	}
 	
 	
