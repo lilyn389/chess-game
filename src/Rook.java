52cdac9e5bbf0a4587c2f86444a064037a0483f2
Grant Singleton <grantsingleton@tamu.edu>
2019-03-28 00:21:11 -0500

    Update Rook.java

--- src/Rook.java
+++ src/Rook.java
@@ -3,13 +3,10 @@ import javax.swing.ImageIcon;
 public class Rook extends Piece {
 	
 	private ImageIcon rook;
-	private int row;
-	private int column;
+
 	
 	public Rook(String color, String name, int row, int column) {
-		super(color, name);
-		this.row = row;
-		this.column = column;
+		super(color, name, row, column);
 		if (color == "white") {
 			rook = new ImageIcon("white_castle.png");
 		}
@@ -25,26 +22,10 @@ public class Rook extends Piece {
 	public void setIcon(ImageIcon rook) {
 		this.rook = rook;
 	}
-
-	public int getRow() {
-		return row;
-	}
-
-	public void setRow(int row) {
-		this.row = row;
-	}
-
-	public int getColumn() {
-		return column;
-	}
-
-	public void setColumn(int column) {
-		this.column = column;
-	}
 	
 	public boolean isValidMove(int x, int y) {
 	
-		return false;
+		return true;
 	}
 	
 	
