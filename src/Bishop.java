1a432cc83ab5fee15c135f52a3c84a337330b0ed
Grant Singleton <grantsingleton@tamu.edu>
2019-03-28 00:18:13 -0500

    Update Bishop.java

--- src/Bishop.java
+++ src/Bishop.java
@@ -3,13 +3,9 @@ import javax.swing.ImageIcon;
 public class Bishop extends Piece {
 	
 	private ImageIcon bishop;
-	private int row;
-	private int column;
 	
 	public Bishop(String color, String name, int row, int column) {
-		super(color, name);
-		this.row = row;
-		this.column = column;
+		super(color, name, row, column);
 		if (color == "white") {
 			bishop = new ImageIcon("white_bishop.png");
 		}
@@ -25,26 +21,10 @@ public class Bishop extends Piece {
 	public void setRookIcon(ImageIcon bishop) {
 		this.bishop = bishop;
 	}
-
-	public int getRow() {
-		return row;
-	}
-
-	public void setRow(int row) {
-		this.row = row;
-	}
-
-	public int getColumn() {
-		return column;
-	}
-
-	public void setColumn(int column) {
-		this.column = column;
-	}
 	
 	public boolean isValidMove(int x, int y) {
 	
-		return false;
+		return true;
 	}
 	
 	
