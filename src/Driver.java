dcb23e338afad7c225f4ddcfdae91544d065d3a0
Oanh Nguyen <onguyen@tamu.edu>
2019-03-29 17:24:14 -0500

    Menu screen

--- src/Driver.java
+++ src/Driver.java
@@ -4,8 +4,9 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		board chess_gui = new board();
-		chess_gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        SplashMenu game = new Menu();
+		//board chess_gui = new board();
+		//chess_gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
