aac3e6d9218a583835aa1e6cfeb2f1598efa1ed6
LCroc99 <lcroc99@tamu.edu>
2019-04-04 23:35:40 -0500

    Add files via upload

---
+++ src/src/Bishop.java
@@ -0,0 +1,29 @@
+import javax.swing.ImageIcon;
+
+public class Bishop extends Piece {
+	
+	private ImageIcon bishop;
+	
+	public Bishop(String color, String name, int row, int column, int ID) {
+		super(color, name, row, column, ID);
+		if (color == "white") {
+			bishop = new ImageIcon("white_bishop.png");
+		}
+		else {
+			bishop = new ImageIcon("black_bishop.png");
+		}
+	}
+
+	public ImageIcon getIcon() {
+		return bishop;
+	}
+
+	public void setRookIcon(ImageIcon bishop) {
+		this.bishop = bishop;
+	}
+	
+	public boolean isValidMove(int x, int y) {
+	
+		return diagonalMove(x, y);
+	}
+}
---
+++ src/src/Queen.java
@@ -0,0 +1,37 @@
+import javax.swing.ImageIcon;
+
+public class Queen extends Piece {
+	
+	private ImageIcon queen;
+	
+	public Queen(String color, String name, int row, int column, int ID) {
+		super(color, name, row, column, ID);
+		if (color == "white") {
+			queen = new ImageIcon("white_queen.png");
+		}
+		else {
+			queen = new ImageIcon("black_queen.png");
+		}
+	}
+
+	public ImageIcon getIcon() {
+		return queen;
+	}
+
+	public void setRookIcon(ImageIcon queen) {
+		this.queen = queen;
+	}
+	
+	public boolean isValidMove(int x, int y) {
+	
+		//Check for path obstructions
+		
+		return validQueenMove(x, y);
+	}
+	
+	private boolean validQueenMove(int x, int y) {
+
+		return orthagonalMove(x, y) || diagonalMove(x, y);
+	}
+	
+}
