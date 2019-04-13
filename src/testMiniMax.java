b301645dbf16a7f4863ab4a83e29150bc778abcc
joshuaandersonj <Joshuaandersonj@tamu.edu>
2019-04-12 23:34:59 -0600

    Add files via upload

---
+++ src/testMiniMax.java
@@ -0,0 +1,10 @@
+
+
+import javax.swing.JFrame;
+
+public class testMiniMax {
+	//board chess_gui = new board();
+	//chess_gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//MiniMaxAlphaBeta tree = new MiniMaxAlphaBeta(chess_gui,"white",3);
+	//Move a = tree.MM();
+}
